9b87184af403cd779eb7e0651cb6e2425281e2c6
Ngufor-emmanuella <emmanuellangufor@gmail.com>
2022-06-30 21:05:50 +0100

    Add home page

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const ham = document.getElementById('ham');
-const div = document.querySelector('hide');
-ham.addEvenListerner('click', () => {
-  div.classList.remove('hide');
-  ham.classList.add('hide');
-  document.querySelector('.box').classList.remove('blur');
+const mobileNav = document.getElementById('hide')
+ham.addEventListener('click', () => {
+  mobileNav.style.left = '0';
+ mobileNav.style.right = '0';
 });
 
-const links =document.querySelectorAll('.navlink');
-links.forEach((link) => {
-  link.addEventListerner('click', () => {
-    div.classList.add('hide');
-    ham.classList.remove('hide');
-    document.querySelector('.box').classList.remove('blur');
-  })
+// const links =document.querySelectorAll('.navlink');
+// links.forEach((link) => {
+//   link.addEventListerner('click', () => {
+//     div.classList.add('hide');
+//     ham.classList.remove('hide');
+//     document.querySelector('.box').classList.remove('blur');
+//   })
+// })
+
+// This will listen for clicks on the X button
+const closebtn = document.querySelector('.markx');
+closebtn.addEventListener('click', () => {
+ mobileNav.style.left = '-100vw';
+ mobileNav.style.right = '100vw';
 })
